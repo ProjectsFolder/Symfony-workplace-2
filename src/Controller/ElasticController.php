53bba895f1bef8d4eafefdd2b6dbfb823b06b689
asylum29 <ksenon3@mail.ru>
2021-10-20 15:44:17 +0700

    working with es

--- src/Controller/ElasticController.php
+++ src/Controller/ElasticController.php
@@ -5,12 +5,20 @@ namespace App\Controller;
 use App\Entity\Contract;
 use App\Entity\Tariff;
 use App\Services\BGBilling;
+use Elastica\Client;
+use Elastica\Document;
+use Elastica\Query;
 use Elastica\Query\BoolQuery;
 use Elastica\Query\MatchQuery;
+use Elastica\Query\Wildcard;
+use Elastica\Request;
+use Elastica\Search;
+use Elastica\Type\Mapping;
 use FOS\ElasticaBundle\Finder\TransformedFinder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
 
 /**
  * @Route("/elastic", name="elastic_")
@@ -105,5 +113,144 @@ class ElasticController extends AbstractController
         }
 
         return $this->json($results);
+    }
+
+    /**
+     * @Route("/ruflin1/{name}/{address}", name="ruflin1")
+     *
+     * @param string $name
+     * @param string $address
+     *
+     * @return Response
+     */
+    public function ruflin1(string $name, string $address): Response
+    {
+        $client = new Client(['url' => $this->getParameter('elastica_url')]);
+        $index = $client->getIndex('contracts');
+        $type = $index->getType('contract');
+        $query = [
+            'query' => [
+                'bool' => [
+                    'must' => [
+                        [
+                            'match' => [
+                                'fullName' => $name,
+                            ],
+                        ],
+                        [
+                            'match' => [
+                                'address' => $address,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $path = $index->getName() . '/' . $type->getName() . '/_search';
+        $response = $client->request($path, Request::GET, $query);
+
+        return $this->json($response->getData());
+    }
+
+    /**
+     * @Route("/ruflin2/{name}/{address}", name="ruflin2")
+     *
+     * @param string $name
+     * @param string $address
+     *
+     * @return Response
+     */
+    public function ruflin2(string $name, string $address): Response
+    {
+        $client = new Client(['url' => $this->getParameter('elastica_url')]);
+
+        $matchQuery1 = new MatchQuery();
+        $matchQuery1->setField('fullName', $name);
+        $matchQuery2 = new MatchQuery();
+        $matchQuery2->setField('address', $address);
+        $boolQuery = new BoolQuery();
+        $boolQuery->addMust([$matchQuery1, $matchQuery2]);
+        $query = new Query($boolQuery);
+
+        $contracts = [];
+        $search = new Search($client);
+        $result = $search->addIndex('contracts')->search($query, 10);
+        foreach ($result as $item) {
+            $contracts[] = $item->getData();
+        }
+
+        return $this->json($contracts);
+    }
+
+    /**
+     * @Route("/ruflin3", name="ruflin3")
+     *
+     * @param HttpRequest $request
+     *
+     * @return Response
+     */
+    public function ruflin3(HttpRequest $request): Response
+    {
+        $client = new Client(['url' => $this->getParameter('elastica_url')]);
+
+        $index = $client->getIndex('test_index');
+        if (!$index->exists()) {
+            $index->create();
+        } elseif ($request->query->has('reload')) {
+            $index->delete();
+            $index->create();
+        }
+
+        $type = $index->getType('test_type');
+        if (!$type->exists()) {
+            $mapping = new Mapping();
+            $mapping->setType($type);
+            $mapping->setProperties([
+                'id' => ['type' => 'integer'],
+                'user' => [
+                    'type' => 'object',
+                    'properties' => [
+                        'name'      => ['type' => 'text'],
+                        'fullName'  => ['type' => 'text', 'boost' => 2],
+                    ],
+                ],
+                'msg' => ['type' => 'text', 'analyzer' => 'russian'],
+            ]);
+            $mapping->send();
+
+            for ($i = 1; $i < 15; ++$i) {
+                $id = time();
+                $item = [
+                    'id' => $id,
+                    'user' => [
+                        'name' => "user_$id",
+                        'fullName' => "fullName_$i"
+                    ],
+                    'msg' => 'Привет, умный поиск!'
+                ];
+                $document = new Document($id, $item);
+                $type->addDocument($document);
+                sleep(1);
+            }
+
+            $type->getIndex()->refresh();
+        }
+
+        $wildcardQuery = new Wildcard();
+        $wildcardQuery->setValue('user.name', 'user_*');
+        $matchQuery = new MatchQuery();
+        $matchQuery->setField('msg', 'умнейшие');
+        $boolQuery = new BoolQuery();
+        $boolQuery->addMust([$wildcardQuery, $matchQuery]);
+        $query = new Query($boolQuery);
+
+        $items = [];
+        $search = new Search($client);
+        $result = $search->addIndex('test_index')->search($query, 10);
+        foreach ($result as $item) {
+            $items[] = $item->getData();
+        }
+
+        return $this->json($items);
     }
 }
